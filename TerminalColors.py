6d91722841f787ed9f42e2eee2243ea69f5157c9
XD_CZ <mandlemankiller@email.cz>
2022-10-06 15:57:26 +0200

    Move terminal colors to new file

---
+++ TerminalColors.py
@@ -0,0 +1,25 @@
+# Local imports
+import Constants
+
+class Color: # Terminal colors
+    Default = "\033[39m"
+    Black = "\033[30m"
+    Red = "\033[31m"
+    Green = "\033[32m"
+    Info = "\033[32m\033[1m[INFO] \033[34m"
+    Yellow = "\033[33m"
+    Blue = "\033[34m"
+    Magenta = "\033[35m"
+    Cyan = "\033[36m"
+    LightGray = "\033[37m"
+    DarkGray = "\033[90m"
+    LightRed = "\033[91m"
+    LightGreen = "\033[92m"
+    LightYellow = "\033[93m"
+    LightBlue = "\033[94m"
+    LightMagenta = "\033[95m"
+    LightCyan = "\033[96m"
+    White = "\033[97m"
+    Bold = "\033[1m"
+    Underlined = "\033[4m"
+    END = '\033[0m'
